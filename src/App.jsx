3a68528d07ee015d597046ddb9f2b860469e392d
maxarvid <max.arvid.anderson@gmail.com>
2021-09-06 14:09:58 +0200

    adds example of old fashioned constructor to set state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     planet: 'Uranus'
+  //   }
+  // }
+  state = {
+    planet: 'Venus'
+  }
   render() {
+    //const planet = "Mars"
+    const {planet} = this.state
     return (
       <div>
-        <h1>Hello World from class component</h1>
+        <h1>Hello {planet} from class component</h1>
       </div>
     )
   }
